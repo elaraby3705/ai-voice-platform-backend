cc318b95bb38b3b4cf8e35c6030f34f1ad2a4ea3
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-17 05:22:10 +0200

    env example and settings.py modification with requested libs to make migration go through

--- AI_Voice_Platform/settings.py
+++ AI_Voice_Platform/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+load_dotenv()  # read .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,11 +78,15 @@ WSGI_APPLICATION = 'AI_Voice_Platform.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# PostgreSQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
