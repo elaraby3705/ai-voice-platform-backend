0a835b321a0f2d7837f2920d648f4f01ce244a84
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-21 00:33:40 +0200

    Url login mapping

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RegisterView
 
 urlpatterns = [
-    # path('login/', views.LoginView.as_view(), name='auth-login'),
     # path('logout/', views.LogoutView.as_view(), name='auth-logout'),
     # path('me/', views.MeView.as_view(), name='auth-me'),
     path("register/", RegisterView.as_view(), name="auth-register"),
+    path('login/', LoginView.as_view(), name='auth-login'),
 ]
