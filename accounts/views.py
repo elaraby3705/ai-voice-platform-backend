4cb752f02b039e5ca4aa65cb25596d11e154c825
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-23 02:43:59 +0200

    Udating project dependancies : also completing Implement Protected User Detail Endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import RegistrationSerializer ,LoginSerializer , UserSerializer
 
 class RegisterView(APIView):
