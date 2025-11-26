e056e31ddbd8597a637bc1c33d2e7572c12ba58d
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-11-26 20:38:01 +0200

    Projects/ models DB

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.CASCADE,
+        related_name="projects"
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.name
+    
